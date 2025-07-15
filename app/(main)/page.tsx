2fb0c8b8bd33e62961c35c4fea62a1215bcd7123
mistrypavankumar <pavansharma.m0114si@gmail.com>
2025-07-15 00:22:20 -0400

    create home page with navbar and background

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,23 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the Rent Management System</h1>
-      <p>Manage your properties and tenants efficiently.</p>
+    <div className="bg-[linear-gradient(90deg,rgba(0,0,0,0.9),rgba(0,0,0,0)),url('/assets/banner.jpg')] bg-cover bg-center h-screen w-full">
+      <div className="w-[90%] mx-auto pt-[15em] h-screen">
+        <div>
+          <h1 className="text-5xl md:text-7xl font-bold text-white mb-4">
+            Welcome to the <br /> Rent Management System
+          </h1>
+          <p className="text-lg text-white mb-4">
+            Streamline your rental operations with ease and confidence.
+          </p>
+          <p className="text-lg text-white mb-8">
+            Sign up today to take full control of your rental properties and
+            simplify your management tasks.
+          </p>
+          <button className="bg-white text-black py-2 px-6 rounded-full">
+            Register Now
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,72 @@
 import Link from "next/link";
 import React from "react";
+import { RiMenu2Line } from "react-icons/ri";
 
 const Navbar = () => {
+  const linkStyle = "flex py-[1em] px-[2em]";
   return (
     <div>
-      <div>
-        <Link href={"/"} className="text-xl">
-          MyRental <span className="text-primary">Hub</span>
-        </Link>
-      </div>
+      <nav className="absolute top-0 z-10 w-full">
+        <ul className="list-none flex items-center justify-between w-[90%] mx-auto py-2">
+          <div className="flex items-center justify-between w-full md:w-auto">
+            <li className="flex">
+              <Link
+                href="/"
+                className={`${linkStyle} pl-0 text-xl font-bold text-white`}
+              >
+                MyRental <span className="">-Hub</span>
+              </Link>
+            </li>
+            <li className="md:hidden flex items-center cursor-pointer">
+              <RiMenu2Line className="text-2xl" />
+            </li>
+          </div>
+          <div className="items-center hidden md:flex">
+            <li className="flex">
+              <Link
+                href="/"
+                className={`${linkStyle} font-semibold text-white`}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="flex">
+              <Link
+                href="/#"
+                className={`${linkStyle} font-semibold text-white`}
+              >
+                About
+              </Link>
+            </li>
+            <li className="flex">
+              <Link
+                href="/#"
+                className={`${linkStyle} font-semibold text-white`}
+              >
+                Contact
+              </Link>
+            </li>
+          </div>
+          <div className="gap-5 hidden md:flex">
+            <li className="flex">
+              <Link
+                href="/login"
+                className={`bg-white border-2 border-white px-[2em] py-[0.5em] rounded-full text-black`}
+              >
+                Login
+              </Link>
+            </li>
+            <li className="flex">
+              <Link
+                href="/register"
+                className={`bg-transparent border-2 border-white px-[2em] py-[0.5em] rounded-full text-white hover:bg-white hover:text-black transition-colors duration-300`}
+              >
+                Register
+              </Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
     </div>
   );
 };
