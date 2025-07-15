f9fb8f09da62442115ca53781ec238e6ff2bc2f1
mistrypavankumar <pavansharma.m0114si@gmail.com>
2025-07-15 00:32:10 -0400

    changed app route structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,28 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="bg-[linear-gradient(90deg,rgba(0,0,0,0.9),rgba(0,0,0,0)),url('/assets/banner.jpg')] bg-cover bg-center h-screen w-full">
       <div className="w-[90%] mx-auto pt-[15em] h-screen">
         <div>
           <h1 className="text-5xl md:text-7xl font-bold text-white mb-4">
-            Welcome to the <br /> Rent Management System
+            Welcome to <br />
+            <span className="text-white">MyRental-Hub</span>
           </h1>
           <p className="text-lg text-white mb-4">
-            Streamline your rental operations with ease and confidence.
+            Hassle-free rent tracking and roommate splitting—made simple.
           </p>
           <p className="text-lg text-white mb-8">
-            Sign up today to take full control of your rental properties and
-            simplify your management tasks.
+            Manage rent, utilities, and payments effortlessly with your
+            housemates. <br /> Keep everything fair and transparent in one
+            place.
           </p>
-          <button className="bg-white text-black py-2 px-6 rounded-full">
+          <Link
+            href="/register"
+            className="bg-white text-black py-3 px-6 rounded-full cursor-pointer"
+          >
             Register Now
-          </button>
+          </Link>
         </div>
       </div>
     </div>
